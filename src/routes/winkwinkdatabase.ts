210af92a65c414c9bd9bd518d94204e63513af71
= <=>
2024-08-14 15:37:20 +0300

    Started feature

--- src/routes/winkwinkdatabase.ts
+++ src/routes/winkwinkdatabase.ts
@@ -1,10 +1,1 @@
-import { Reason, type QuestionsCollection } from "./types"
-
-let DATABASE = [ ]
-
-function updateDatabase(newDatabase: QuestionsCollection[]) {
-    DATABASE = newDatabase;
-}
-
-export { DATABASE, updateDatabase };
 
